49c42355b455285ae8b93adf99cd2520756abda6
Ian W <ian.uu12@gmail.com>
2017-01-08 14:46:25 -0800

    Basic robot

---
+++ src/org/usfirst/frc/team4131/robot/RobotMap.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team4131.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap{
+	
+}
