a161c3fd8bcd88446036072401da89447965bb67
Matthew Oberg <matthewroberg@gmail.com>
2017-01-28 14:26:45 -0800

    Upaded Talon library

--- src/org/usfirst/frc/team4131/robot/subsystems/Hopper.java
+++ src/org/usfirst/frc/team4131/robot/subsystems/Hopper.java
@@ -2,7 +2,8 @@ package org.usfirst.frc.team4131.robot.subsystems;
 
 import org.usfirst.frc.team4131.robot.RobotMap;
 
-import edu.wpi.first.wpilibj.CANTalon;
+import com.ctre.CANTalon;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Hopper extends Subsystem {
