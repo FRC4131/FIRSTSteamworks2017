6af80d1424f1f24fdfc8d09b1cc04dbfaa32e12b
treecal20 <treecal20@gmail.com>
2017-01-27 18:59:49 -0800

    Blerg

--- src/org/usfirst/frc/team4131/robot/RobotMap.java
+++ src/org/usfirst/frc/team4131/robot/RobotMap.java
@@ -20,31 +20,7 @@ public class RobotMap {
 	/**
 	 * Configuration for the RoboRio(s)
 	 */
-	public static final String ELECTRICAL_BOT = "ElectricalBot"; //0
-	public static final int ELECT_BOT_NUM = 0;
 
-	public static final String PRACTICE_BOT = "PracticeBot";//1
-	public static final int PRAC_BOT_NUM = 1;
-
-	public static final String COMP_BOT = "CompetitionBot"; //2
-	public static final int COMP_BOT_NUM = 2;
-
-	public static final String CONFIG_FILENAME = "WHOAMI.TXT";
-
-	public static int robotType(String robot) {
-		if (robot.equals(ELECTRICAL_BOT)) {
-			return 0;
-		} else if (robot.equals(PRACTICE_BOT)) {
-			return 1;
-		} else if (robot.equals(COMP_BOT)) {
-			return 2;
-		} else {
-			return -1;
-		}
-	}
-	
-	public static int ROBOT_TYPE = -1;
-	
 	public static final int LEFT_JOYSTICK = 0;
 
 	public static final int RIGHT_JOYSTICK = 1;
@@ -52,6 +28,7 @@ public class RobotMap {
 	public static final int HOPPER_MOTOR = 9;
 	
 	public static final int FLYWHEEL_MOTOR = 11;
+
 
 	public static final int DRIVE_LEFT_MOTOR1 = 0;
 
@@ -68,6 +45,4 @@ public class RobotMap {
 	public static final int DRIVE_RIGHT_ENCODERA = 2;
 
 	public static final int DRIVE_RIGHT_ENCODERB = 3;
-	
-	public static final double DRIVE_RATIO = 1;// = ((125D + 3D / 16D) / 1033D);
 }
