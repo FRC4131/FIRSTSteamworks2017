c2a2192b9482669b0b6469b7710607785300f8a0
alex <redranger003@gmail.com>
2017-01-31 15:55:48 -0800

    started climber comand

---
+++ src/org/usfirst/frc/team4131/robot/commands/Climber.java
@@ -0,0 +1,28 @@
+package org.usfirst.frc.team4131.robot.commands;
+
+import org.usfirst.frc.team4131.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class Climber extends Command{
+	public Climber() {
+		requires(Robot.climber);
+	}
+	protected void initialize(){
+		
+	}
+	protected void execute(){
+		
+	}
+	@Override 
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	protected void end(){
+		
+	}
+	protected void interrupted(){
+		
+	}
+}
