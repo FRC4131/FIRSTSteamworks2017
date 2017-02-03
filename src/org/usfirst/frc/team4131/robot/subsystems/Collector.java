39f189063e6ca9aeac907d18dc604a452985230e
Iron Patriots <lhsfrc4131@gmail.com>
2017-02-03 15:16:36 -0800

    Collector system

--- src/org/usfirst/frc/team4131/robot/subsystems/Collector.java
+++ src/org/usfirst/frc/team4131/robot/subsystems/Collector.java
@@ -1,12 +1,41 @@
 package org.usfirst.frc.team4131.robot.subsystems;
 
+import org.usfirst.frc.team4131.robot.RobotMap;
+
+import com.ctre.CANTalon;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+/**
+ * ========== Test Procedure ========== Run on Robot in a Box We will call
+ * {@link #engage()} in teleop, and expect the motor to spin at half speed. We
+ * will call {@link #climb()} in teleop, and expect the motor to spin at full
+ * speed in the same direction as {{@link #engage()}. Tests not passed
+ * ====================================
+ * 
+ * @author Patrick
+ * @since 1/28/2017
+ * 
+ */
 public class Collector extends Subsystem {
 
 	@Override
 	protected void initDefaultCommand() {
-		// TODO Auto-generated method stub
+	}
+
+	// TODO Auto-generated method stub
+	CANTalon motor;
+
+	public Collector() {
+		motor = new CANTalon(RobotMap.COLLECTOR_MOTOR);
+	}
+
+	public void collect() {
+		motor.set(1);
+	}
+
+	public void stop() {
+		motor.set(0);
 
 	}
 
