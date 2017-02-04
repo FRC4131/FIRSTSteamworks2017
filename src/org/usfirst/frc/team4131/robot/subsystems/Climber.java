d5a8da4037c547498b085abc9d48b3c7afe79d96
Ian W <ian.uu12@gmail.com>
2017-02-04 15:18:48 -0800

    Un-edit OI and Climber

--- src/org/usfirst/frc/team4131/robot/subsystems/Climber.java
+++ src/org/usfirst/frc/team4131/robot/subsystems/Climber.java
@@ -18,22 +18,17 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Climber extends Subsystem{
 	private CANTalon motor;
-	
 	public Climber(){
 		motor = new CANTalon(RobotMap.CLIMBER_MOTOR);
 	}
-	
 	@Override
 	protected void initDefaultCommand(){}
-	
 	public void engage(){
 		motor.set(0.5);
 	}
-	
 	public void climb(){
 		motor.set(1);
 	}
-	
 	public void stop(){
 		motor.set(0);
 	}
