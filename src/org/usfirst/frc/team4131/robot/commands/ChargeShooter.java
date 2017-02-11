c1b1d153f233832d8583e18efa11d11527d3841e
Ian W <ian.uu12@gmail.com>
2017-02-10 17:08:25 -0800

    Test

--- src/org/usfirst/frc/team4131/robot/commands/ChargeShooter.java
+++ src/org/usfirst/frc/team4131/robot/commands/ChargeShooter.java
@@ -4,6 +4,16 @@ import org.usfirst.frc.team4131.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
 
+/**
+ * ========== Test Procedure ==========
+ * Run on Robot in a Box
+ * I will press button one on the left joystick, and one of the motors will move at full speed.
+ * Tests passed
+ * ====================================
+ * @author Matthew
+ * @since 2/10/2017
+ */
+
 public class ChargeShooter extends Command {
 	
 	protected void initialize() {
