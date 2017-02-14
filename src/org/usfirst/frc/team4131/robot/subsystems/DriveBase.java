61b51064c838b49a5082aad1667d2ac6c37c2ca0
treecal20 <treecal20@gmail.com>
2017-02-13 16:06:45 -0800

    Update DriveBase.java

--- src/org/usfirst/frc/team4131/robot/subsystems/DriveBase.java
+++ src/org/usfirst/frc/team4131/robot/subsystems/DriveBase.java
@@ -15,24 +15,16 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  * @since 2/4/2017
  */
 public class DriveBase extends Subsystem {
-	private CANTalon leftMotor1, leftMotor2, leftMotor3, rightMotor1, rightMotor2, rightMotor3;
+	private CANTalon leftMotor1, rightMotor1;
 	public DriveBase(){
 		leftMotor1 = new CANTalon(RobotMap.DRIVE_LEFT1);
-		leftMotor2 = new CANTalon(RobotMap.DRIVE_LEFT2);
-		leftMotor3 = new CANTalon(RobotMap.DRIVE_LEFT3);
 		rightMotor1 = new CANTalon(RobotMap.DRIVE_RIGHT1);
-		rightMotor2 = new CANTalon(RobotMap.DRIVE_RIGHT2);
-		rightMotor3 = new CANTalon(RobotMap.DRIVE_RIGHT3);
 	}
 	protected void initDefaultCommand(){
 		
 	}
 	public void move(double speed1, double speed2) {
 		leftMotor1.set(-speed1);
-		leftMotor2.set(-speed1);
-		leftMotor3.set(-speed1);
 		rightMotor1.set(-speed2);
-		rightMotor2.set(-speed2);
-		rightMotor3.set(-speed2);
 	}
 }
