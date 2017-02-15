4e8192c009d17ed7f6f2c1c826c64241d87627af
Ian W <ian.uu12@gmail.com>
2017-02-14 20:00:04 -0800

    Working pneumatics

--- src/org/usfirst/frc/team4131/robot/commands/SpitOut.java
+++ src/org/usfirst/frc/team4131/robot/commands/SpitOut.java
@@ -1,8 +1,6 @@
 package org.usfirst.frc.team4131.robot.commands;
 
 import org.usfirst.frc.team4131.robot.Robot;
-import org.usfirst.frc.team4131.robot.RobotMap;
-
 
 import edu.wpi.first.wpilibj.command.Command;
 /**
@@ -35,8 +33,7 @@ public class SpitOut extends Command {
 
 	@Override
 	protected boolean isFinished() {
-		// TODO Auto-generated method stub
-		return !Robot.OI.getSpitOut();
+		return false;
 	}
 
 	@Override
