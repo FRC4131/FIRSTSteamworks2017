2cccb940a94fafb5c45a1131406210fbecef3082
treecal20 <treecal20@gmail.com>
2017-02-14 18:08:04 -0800

    Climber

---
+++ src/org/usfirst/frc/team4131/robot/commands/EngageClimber.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team4131.robot.commands;
+
+import org.usfirst.frc.team4131.robot.Robot;
+import org.usfirst.frc.team4131.robot.RobotMap;
+
+import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class EngageClimber extends Command {
+	CANTalon motor;
+	@Override
+	protected void initialize() {
+		// TODO Auto-generated method stub
+		motor = new CANTalon(RobotMap.CLIMBER_MOTOR);
+	}
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+		motor.set(0.5);
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return !Robot.OI.getEngageClimb();
+	}
+
+	@Override
+	protected void end() {
+		// TODO Auto-generated method stub
+		motor.set(0);
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+		motor.set(0);
+	}
+
+}
