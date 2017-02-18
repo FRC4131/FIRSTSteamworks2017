fedc6c9c41d7998aca7feebbc05d1bfd9f34ea62
Ian W <ian.uu12@gmail.com>
2017-02-18 10:56:55 -0800

    Update test comments

--- src/org/usfirst/frc/team4131/robot/commands/Shoot.java
+++ src/org/usfirst/frc/team4131/robot/commands/Shoot.java
@@ -7,35 +7,30 @@ import edu.wpi.first.wpilibj.command.Command;
 /**
 * ========== Test Procedure ==========
 * Run on Robot in a Box
-* We will bind this command to a button in the OI and expect the hopper to run when it is pressed.
+* We will start this command in teleopInit and expect the hopper to run while the robot is enabled in teleop.
 * Tests passed
 * ====================================
 * @author Ian
-* @since 2/4/2017
+* @since 2/18/2017
 */
 public class Shoot extends Command{
 	public Shoot(){
 		requires(Robot.hopper);
 	}
-	
 	@Override
 	protected void initialize(){}
-	
 	@Override
 	protected void execute(){
 		Robot.hopper.run();
 	}
-	
 	@Override
 	protected boolean isFinished(){
 		return false;
 	}
-	
 	@Override
 	protected void end(){
 		Robot.hopper.stop();
 	}
-	
 	@Override
 	protected void interrupted(){
 		Robot.hopper.stop();
