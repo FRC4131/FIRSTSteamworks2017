59fb55e8b544f2ef8220eb4525d2eecced1f30c9
treecal20 <treecal20@gmail.com>
2017-02-18 12:39:06 -0800

    Update EngageClimber.java

--- src/org/usfirst/frc/team4131/robot/commands/EngageClimber.java
+++ src/org/usfirst/frc/team4131/robot/commands/EngageClimber.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.command.Command;
 /**
  * ========== Test Procedure ==========
  * Ran on Robot in a Box
- * We will enable teleop, and expect to turn at half speed.
+ * We will enable teleop, and expect the motor to turn at half speed.
  * Test passed
  * ====================================
  * @author Calvin
@@ -20,30 +20,25 @@ public class EngageClimber extends Command {
 	}
 	@Override
 	protected void initialize() {
-		// TODO Auto-generated method stub
 	}
 
 	@Override
 	protected void execute() {
-		// TODO Auto-generated method stub
 		Robot.climber.engage();
 	}
 
 	@Override
 	protected boolean isFinished() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	protected void end() {
-		// TODO Auto-generated method stub
 		Robot.climber.stop();
 	}
 
 	@Override
 	protected void interrupted() {
-		// TODO Auto-generated method stub
 		Robot.climber.stop();
 	}
 
