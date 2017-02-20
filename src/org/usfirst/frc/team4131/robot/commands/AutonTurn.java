fcb1807457d91ba9ddcc4b329e286cc5c95435e8
Matthew Oberg <matthewroberg@gmail.com>
2017-02-19 19:11:40 -0800

    Auton

---
+++ src/org/usfirst/frc/team4131/robot/commands/AutonTurn.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team4131.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class AutonTurn extends Command {
+
+	protected boolean isFinished() {
+		return false;
+	}
+
+}
--- src/org/usfirst/frc/team4131/robot/subsystems/DriveBase.java
+++ src/org/usfirst/frc/team4131/robot/subsystems/DriveBase.java
@@ -4,6 +4,7 @@ import org.usfirst.frc.team4131.robot.RobotMap;
 import org.usfirst.frc.team4131.robot.commands.Move;
 
 import com.ctre.CANTalon;
+import com.ctre.CANTalon.TalonControlMode;
 import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
@@ -25,19 +26,24 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  * @since 2/17/2017
  */
 public class DriveBase extends Subsystem {
-	CANTalon[] left = new CANTalon[3];
-	CANTalon[] right = new CANTalon[3];
+	CANTalon[] leftMotors = new CANTalon[3];
+	CANTalon[] rightMotors = new CANTalon[3];
 	private DoubleSolenoid leftShifter, rightShifter;
 	private Encoder leftEncoder, rightEncoder;
 	private AHRS imu;
 	public DriveBase(){
 		for(int i=0; i<3; i++){
-			left[i] = new CANTalon(RobotMap.left[i]);
-			right[i] = new CANTalon(RobotMap.right[i]);
+			leftMotors[i] = new CANTalon(RobotMap.left[i]);
+			rightMotors[i] = new CANTalon(RobotMap.right[i]);
+		}
+		for(int i=1; i<3; i++){
+			leftMotors[i].changeControlMode(TalonControlMode.Follower);
+			leftMotors[i].set(RobotMap.left[0]);
+			rightMotors[i].changeControlMode(TalonControlMode.Follower);
+			rightMotors[i].set(RobotMap.right[0]);
 		}
 		leftShifter = new DoubleSolenoid(RobotMap.PCM_ID, RobotMap.LEFT_SHIFTER1, RobotMap.LEFT_SHIFTER2);
 		rightShifter = new DoubleSolenoid(RobotMap.PCM_ID, RobotMap.RIGHT_SHIFTER1, RobotMap.RIGHT_SHIFTER2);
-		
 		leftEncoder = new Encoder(RobotMap.ENCODER_LEFT1, RobotMap.ENCODER_LEFT2);
 		rightEncoder = new Encoder(RobotMap.ENCODER_RIGHT1, RobotMap.ENCODER_RIGHT2);
 		leftEncoder.setDistancePerPulse(RobotMap.DRIVE_INCHES_PER_PULSE);
@@ -49,8 +55,8 @@ public class DriveBase extends Subsystem {
 		setDefaultCommand(new Move());
 	}
 	public void move(double left, double right) {
-		leftMotor.set(left);
-		rightMotor.set(right);
+		leftMotors[0].set(left);
+		rightMotors[0].set(right);
 	}
 	public void shiftUp(){
 		leftShifter.set(DoubleSolenoid.Value.kForward);
