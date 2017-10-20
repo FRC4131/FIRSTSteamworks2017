9ea1e40f0cd2cacc4822c469758bfa629d797720
Matthew Oberg <matthewroberg@gmail.com>
2017-10-19 17:02:11 -0700

    Rewrite GearPocket.java

--- src/org/usfirst/frc/team4131/robot/subsystems/GearPocket.java
+++ src/org/usfirst/frc/team4131/robot/subsystems/GearPocket.java
@@ -1,22 +1,24 @@
 package org.usfirst.frc.team4131.robot.subsystems;
 
 import org.usfirst.frc.team4131.robot.RobotMap;
-
-import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class GearPocket extends Subsystem{
-	private DoubleSolenoid solenoid;
+	private Solenoid solenoid1;
+	private Solenoid solenoid2;
 	public GearPocket(){
-		solenoid = new DoubleSolenoid(RobotMap.PCM_ID, RobotMap.GEAR_POCKET1, RobotMap.GEAR_POCKET2);
+		solenoid1 = new Solenoid(RobotMap.PCM_ID, RobotMap.GEAR_POCKET1);
+		solenoid2 = new Solenoid(RobotMap.PCM_ID, RobotMap.GEAR_POCKET2);
 	}
 	protected void initDefaultCommand(){
-		
 	}
 	public void extend(){
-		solenoid.set(DoubleSolenoid.Value.kForward);
+		solenoid1.set(true);
+		solenoid2.set(false);
 	}
 	public void retract(){
-		solenoid.set(DoubleSolenoid.Value.kReverse);
+		solenoid1.set(false);
+		solenoid2.set(true);
 	}
 }
