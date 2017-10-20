65f0ca079bcbb3a26f19d843a5913d5eef25fc76
Matthew Oberg <matthewroberg@gmail.com>
2017-10-19 17:01:28 -0700

    Rewrite Climber.java

--- src/org/usfirst/frc/team4131/robot/subsystems/Climber.java
+++ src/org/usfirst/frc/team4131/robot/subsystems/Climber.java
@@ -1,22 +1,10 @@
 package org.usfirst.frc.team4131.robot.subsystems;
 
 import org.usfirst.frc.team4131.robot.RobotMap;
-
 import com.ctre.CANTalon;
 import com.ctre.CANTalon.TalonControlMode;
-
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-/**
- * ========== Test Procedure ==========
- * Run on Competition Bot
- * We will call {@link #engage()} with the climber directly under the rope, and expect the climber to engage the rope and begin to climb.
- * We will call {@link #climb()} in teleop, and expect the robot to climb the rope.
- * Tests passed
- * ====================================
- * @author Ian
- * @since 1/24/2017
- */
 public class Climber extends Subsystem{
 	private CANTalon motor;
 	public Climber(){
@@ -35,8 +23,5 @@ public class Climber extends Subsystem{
 	}
 	public void stop(){
 		motor.set(0);
-	}
-	public void reverse(){
-		motor.set(-0.7);
 	}
 }
