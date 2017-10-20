dba2d1a97d0bed132feedf21501d12bb56e5d273
Matthew Oberg <matthewroberg@gmail.com>
2017-10-19 17:01:55 -0700

    Rewrite GearClaw.java

--- src/org/usfirst/frc/team4131/robot/subsystems/GearClaw.java
+++ src/org/usfirst/frc/team4131/robot/subsystems/GearClaw.java
@@ -1,22 +1,24 @@
+
 package org.usfirst.frc.team4131.robot.subsystems;
 
 import org.usfirst.frc.team4131.robot.RobotMap;
-
-import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class GearClaw extends Subsystem{
-	private DoubleSolenoid solenoid;
+	private Solenoid solenoid1, solenoid2;
 	public GearClaw(){
-		solenoid = new DoubleSolenoid(RobotMap.PCM_ID, RobotMap.GEAR_CLAW1, RobotMap.GEAR_CLAW2);
+		solenoid1 = new Solenoid(RobotMap.PCM_ID, RobotMap.GEAR_CLAW1);
+		solenoid2 = new Solenoid(RobotMap.PCM_ID, RobotMap.GEAR_CLAW2);
 	}
 	protected void initDefaultCommand(){
-		
 	}
 	public void open(){
-		solenoid.set(DoubleSolenoid.Value.kForward);
+		solenoid1.set(false);
+		solenoid2.set(true);
 	}
 	public void close(){
-		solenoid.set(DoubleSolenoid.Value.kReverse);
+		solenoid1.set(true);		
+		solenoid2.set(false);
 	}
 }
